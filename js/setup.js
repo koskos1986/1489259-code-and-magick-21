45d13e9429cd364fd0bb4da8c728ecc8a4534ff5
koskos1986 <ekosopletkin@yandex.ru>
2020-09-29 14:35:06 +0300

    Исправляет замечания pt.1

--- js/setup.js
+++ js/setup.js
@@ -1,20 +1,20 @@
 'use strict';
 
-let MAGE_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-let MAGE_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-let COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-let EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-let MAGE_QUANTITY = `5`;
+const MAGE_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+const MAGE_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+const COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+const EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+const MAGE_QUANTITY = `4`;
 
-let getRandom = (array) => array[Math.floor(Math.random() * array.length)];
+const getRandomMageFeatures = (array) => array[Math.floor(Math.random() * array.length)];
 
-let getMageData = (number) => {
+let getMage = (number) => {
   let data = [];
   for (var i = 0; i < number; i++) {
     data [i] = {
-      name: getRandom(MAGE_NAME) + ' ' + getRandom(MAGE_SURNAME),
-      coatColor: getRandom(COAT_COLORS),
-      eyeColor: getRandom(EYES_COLORS)
+      name: getRandomMageFeatures(MAGE_NAME) + ' ' + getRandomMageFeatures(MAGE_SURNAME),
+      coatColor: getRandomMageFeatures(COAT_COLORS),
+      eyeColor: getRandomMageFeatures(EYES_COLORS)
     };
   }
   return data;
@@ -31,7 +31,7 @@ let renderMage = (obj) => {
 
 let buildFragment = (array) => {
   let fragment = document.createDocumentFragment();
-  array.forEach(function (item) {
+  array.forEach((item) => {
     fragment.appendChild(renderMage(item));
   });
   return fragment;
@@ -41,7 +41,7 @@ let renderSetup = () => {
   let setupWindow = document.querySelector('.setup');
   let similarMage = setupWindow.querySelector('.setup-similar-list');
   let similarMageBlock = setupWindow.querySelector('.setup-similar');
-  similarMage.appendChild(buildFragment(getMageData(MAGE_QUANTITY)));
+  similarMage.appendChild(buildFragment(getMage(MAGE_QUANTITY)));
   setupWindow.classList.remove('hidden');
   similarMageBlock.classList.remove('hidden');
 };
